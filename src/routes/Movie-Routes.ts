bcb2d1734ff81287cf6adcb2b79d7fbd465bf811
Alex Zaks <zaksa86@gmail.com>
2021-12-21 15:51:58 -0500

    more edits

--- src/routes/Movie-Routes.ts
+++ src/routes/Movie-Routes.ts
@@ -3,36 +3,41 @@ import Movie from "./Movie";
 
 const routes = express.Router();
 
-const Movies: Movie[] = [
+const MoviesArray: Movie[] = [
     { id: 1, title: "Jumanji", year: 1995, animated: false },
     { id: 2, title: "FifthElement", year: 1997, animated: false },
     { id: 3, title: "Back to the Future", year: 1985, animated: false },
     { id: 4, title: "Inception", year: 2010, animated: false },
 ]
 
-routes.get("/", function (request, response) {
-    let minYearParam: string = request.query.minRating as string;
-    if (minYearParam) {
-    let minYear: number = Number.parseFloat(request.query.minYear as string);
-    let filteredMovies: Movie[] = Movies.filter((Movies) => Movies.id >= minYear);
-    response.json(filteredMovies);
+routes.get("/", (req, res) => {
+    let movieMax = Number.parseInt(req.query.maxYear as string);
+    let movieMin = Number.parseInt(req.query.minYear as string);
+    if (movieMax && movieMin) {
+        let found:Movie[] = [];
+        for (let i = 0; i > MoviesArray.length; i++) {
+            if(MoviesArray[i].year > movieMin && MoviesArray[i].year < movieMax){
+                found.push(MoviesArray[i]);
+            }
+        }
+        res.json(found);
     } else {
-        response.json(Movies);
+        res.json(MoviesArray);
     }
-  });
+})
 
 routes.get("/:movie-id", function (req, res) {
-    for (let i = 0; i < Movies.length; i++) {
-      let inputId: number = Number.parseInt(req.params.id)
+    for (let i = 0; i < MoviesArray.length; i++) {
+      let movieID: number = Number.parseInt(req.params.id);
       //find Movie by id property
-      if (Movies[i].id === inputId) {
+      if (MoviesArray[i].id === movieID) {
         //return the json response with the Movie object
-        res.json(Movies[i]);
+        res.json(MoviesArray[i]);
         break;
       }
     }
     res.status(404);
-    res.send({ error: "Shop not found" });
+    res.send({ error: "Movies with id 1 does not exist" });
   });
 
 export default routes;
